42e0262e8d299505f5a8e6682763ed52690cdf99
Andrew Kohanovskij <akohan91@gmail.com>
2020-11-01 16:48:44 +0300

    Changed indent to tab instead of space Changes to be committed: modified:   force-app/main/default/classes/DemoRestService.cls modified:   force-app/main/default/classes/Libak_Map.cls modified:   force-app/main/default/classes/Libak_Processor.cls modified:   force-app/main/default/classes/Libak_ProcessorFactory.cls modified:   force-app/main/default/classes/Libak_Request.cls modified:   force-app/main/default/classes/Libak_Response.cls modified:   force-app/main/default/classes/Libak_RestProcessor.cls modified:   force-app/main/default/classes/Libak_RestRequest.cls modified:   force-app/main/default/classes/Libak_RestResponse.cls modified:   force-app/main/default/classes/Libak_RestService.cls modified:   force-app/main/default/lwc/libak_Base/libak_Base.js modified:   force-app/main/default/lwc/libak_Base/libak_Base.js-meta.xml modified:   force-app/main/default/lwc/libak_notifyBlock/libak_notifyBlock.html modified:   force-app/main/default/lwc/libak_notifyBlock/libak_notifyBlock.js

--- force-app/main/default/lwc/libak_Base/libak_Base.js
+++ force-app/main/default/lwc/libak_Base/libak_Base.js
@@ -2,29 +2,29 @@ import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
 export default class Libak_Base extends LightningElement {
-    /** @property {Boolean} */
-    isBusy;
+	/** @property {Boolean} */
+	isBusy;
 
-    /***********************************************************************************
-     *                                  METHODS
-     ***********************************************************************************/
+	/***********************************************************************************
+	 *                                  METHODS
+	 ***********************************************************************************/
 
-    /**
-     * Method shows the toast message
-     * @param {Object} request { 
-     *  status < 'success', 'error', 'warning', 'info' > 
-     *  title, 
-     *  message, 
-     * }
-     */
-    showToast(request) {
-        const event = new ShowToastEvent({
-            title: request.title,
-            message: request.message,
-            variant: request.status,
-        });
-        this.dispatchEvent(event);
-    }
+	/**
+	 * Method shows the toast message
+	 * @param {Object} request { 
+	 *  status < 'success', 'error', 'warning', 'info' > 
+	 *  title, 
+	 *  message, 
+	 * }
+	 */
+	showToast(request) {
+		const event = new ShowToastEvent({
+			title: request.title,
+			message: request.message,
+			variant: request.status,
+		});
+		this.dispatchEvent(event);
+	}
 
-    
+
 }
